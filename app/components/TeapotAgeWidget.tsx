9bb5f30e6d27111c2e36191cb451f4587dc81e27
Aaron Kalair <aaronkalair@gmail.com>
2016-10-15 14:56:06 +0100

    chore: noticket - TSlint

--- app/components/TeapotAgeWidget.tsx
+++ app/components/TeapotAgeWidget.tsx
@@ -1,5 +1,5 @@
-import React, { PropTypes } from 'react';
-import {Card, CardHeader, CardText} from 'material-ui/Card';
+import React, { PropTypes } from "react";
+import {Card, CardHeader, CardText} from "material-ui/Card";
 
 
 interface TeapotAgeProps {
@@ -8,17 +8,17 @@ interface TeapotAgeProps {
 
 
 const TeapotAge: React.StatelessComponent<TeapotAgeProps> = ({teapotAge}) => (
-    <div className='large-card'>
-    	<Card>
-    		<CardHeader
-      			title="Age of Teapot"
-      			avatar="robot.jpg" />
-      		<CardText>
-      			{Math.round(teapotAge)} minutes old
-      		</CardText>
+    <div className="large-card">
+        <Card>
+            <CardHeader
+                title="Age of Teapot"
+                avatar="robot.jpg" />
+            <CardText>
+                {Math.round(teapotAge)} minutes old
+            </CardText>
         </Card>
     </div>
-)
+);
 
 TeapotAge.propTypes = {
     teapotAge: PropTypes.number.isRequired
--- app/components/TeapotStatusWidget.tsx
+++ app/components/TeapotStatusWidget.tsx
@@ -1,5 +1,5 @@
-import React, { PropTypes } from 'react';
-import {Card,  CardHeader, CardText} from 'material-ui/Card';
+import React, { PropTypes } from "react";
+import {Card,  CardHeader, CardText} from "material-ui/Card";
 
 interface TeapotStatusProps {
     teapotStatus: string;
