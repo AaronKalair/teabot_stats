92be582d94eb5ed8c21f2402ca6a35f0387d64a3
William Cooke <will@bruisyard.eu>
2016-10-31 21:01:23 +0000

    Fixy McFixface for the tests

--- app/components/__tests__/TestTotalNumberOfTeapots.js
+++ app/components/__tests__/TestTotalNumberOfTeapots.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactTestUtils from 'react-addons-test-utils';
+import { IntlProvider } from 'react-intl';
 import TotalNumberOfTeapots from '../TotalNumberOfTeapots';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 it('renders correctly', () => {
     const comp = ReactTestUtils.renderIntoDocument(
-        <MuiThemeProvider><TotalNumberOfTeapots numberOfTeapots={1} /></MuiThemeProvider>
+        <IntlProvider locale="en">
+            <MuiThemeProvider>
+                <TotalNumberOfTeapots numberOfTeapots={1} />
+            </MuiThemeProvider>
+        </IntlProvider>
     );
-    const element = ReactTestUtils.findRenderedDOMComponentWithClass(comp, 'card-margin');
+    const element = ReactTestUtils.findRenderedDOMComponentWithClass(comp,
+        'paper__title');
     expect(element.textContent).toContain('1');
-    expect(element.textContent).toContain('Number of Teapots');
 });
